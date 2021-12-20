ae0f538d721fec5d737f7685aebafc3034c822eb
Italo Silva <italo.s@turing.com>
2021-12-20 09:33:17 -0300

    Day 20: Python - use ints instead of strings

--- 2021/day20/py/day20_v1.py
+++ 2021/day20/py/day20_v1.py
@@ -7,36 +7,34 @@ import numpy as np
 
 def read_problem() -> tuple[np.ndarray, np.ndarray]:
     algo, image_txt = sys.stdin.read().split("\n\n")
-    algo = np.array(list(algo)) == "#"
+    algo = np.where(np.array(list(algo)) == "#", "1", "0")
     image = [list(line.strip()) for line in image_txt.splitlines()]
-    image = np.array(image) == "#"
+    image = np.where(np.array(image) == "#", "1", "0")
     return algo, image
 
 
-def enhance(image: np.ndarray, algo: np.ndarray, fill: int) -> np.ndarray:
+def enhance(image: np.ndarray, algo: np.ndarray, fill: str) -> np.ndarray:
     image = np.pad(image, 2, constant_values=fill)
     n, m = image.shape
-    new = np.full((n, m), fill)
+    new = np.full((n, m), fill, dtype="str")
 
     for i in range(1, n - 1):
         for j in range(1, m - 1):
             neighbours = image[i - 1 : i + 2, j - 1 : j + 2].flatten()
-            idx = 0
-            for bit in neighbours:
-                idx = (idx << 1) | bit
-            new[i, j] = algo[idx]
+            idx = int("".join(neighbours), 2)
+            new[i, j] = str(algo[idx])
 
     return new[1 : n - 1, 1 : m - 1]
 
 
 def solve(image: np.ndarray, algo: np.ndarray, n: int) -> int:
     for i in range(n):
-        if algo[0] == 0:
-            fill = 0
+        if algo[0] == "0":
+            fill = "0"
         else:
-            fill = 0 if i % 2 == 0 else 1
+            fill = "0" if i % 2 == 0 else "1"
         image = enhance(image, algo, fill)
-    return image.sum()
+    return (image == "1").sum()
 
 
 if __name__ == "__main__":
