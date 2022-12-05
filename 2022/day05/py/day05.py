befb5b5472e8c2e7db94c9e0983d71ca0371197a
Italo Silva <italo@maleldil.com>
2022-12-05 09:07:05 +0000

    2022: Day 5 refactor

--- 2022/day05/py/day05.py
+++ 2022/day05/py/day05.py
@@ -8,44 +8,41 @@ def splitstr(s, n=4):
     return [s[i : i + n] for i in range(0, len(s), n)]
 
 
-stacks, instructions = sys.stdin.read().split("\n\n")
-# stacks = stacks.replace("[", "").replace("]", "")
-# print(stacks)
-# stacks_lines = [s.split(" ") for s in stacks.splitlines()[:-1]]
-stacks_lines = [splitstr(s) for s in stacks.splitlines()[:-1]]
-# print(stacks_lines)
-stacks_cols = list(map(list, itertools.zip_longest(*stacks_lines, fillvalue=None)))
-stacks = []
-for col in stacks_cols:
-    new_col = []
-    for x in col:
-        if x is not None and x.strip():
-            # new_col.append(x.replace("]", "").replace("[", ""))
-            new_col.append(re.sub(r"\]|\[", "", x).strip())
-    stacks.append(list(reversed(new_col)))
-
-instrs = [
-    list(map(int, re.findall(r"move (\d+) from (\d+) to (\d+)", i)[0]))
-    for i in instructions.splitlines()
-]
-
-stackscopy = copy.deepcopy(stacks)
-for count, fro, to in instrs:
-    for i in range(count):
-        el = stackscopy[fro - 1].pop()
-        stackscopy[to - 1].append(el)
-
-print("Part 1", "".join(s[-1] for s in stackscopy))
-
-for count, fro, to in instrs:
-    els = stacks[fro - 1][-count:]
-    # print(stacks[fro - 1], stacks[fro - 1])
-    # print(count, fro, to, els)
-    stacks[fro - 1] = stacks[fro - 1][:-count]
-    # print(stacks[fro - 1])
-    stacks[to - 1].extend(els)
-    # print(stacks[to - 1])
-    # print()
+def part1(stacks, instrs):
+    for count, fro, to in instrs:
+        for _ in range(count):
+            el = stacks[fro - 1].pop()
+            stacks[to - 1].append(el)
+    return "".join(s[-1] for s in stacks)
 
 
-print("Part 2", "".join(s[-1] for s in stacks))
+def part2(stacks, instrs):
+    for count, fro, to in instrs:
+        els = stacks[fro - 1][-count:]
+        stacks[fro - 1] = stacks[fro - 1][:-count]
+        stacks[to - 1].extend(els)
+    return "".join(s[-1] for s in stacks)
+
+
+def main():
+    stacks, instructions = sys.stdin.read().split("\n\n")
+    stacks_lines = [splitstr(s) for s in stacks.splitlines()[:-1]]
+    stacks_cols = list(map(list, itertools.zip_longest(*stacks_lines, fillvalue=None)))
+    stacks = []
+    for col in stacks_cols:
+        new_col = []
+        for x in col:
+            if x is not None and x.strip():
+                new_col.append(re.sub(r"\]|\[", "", x).strip())
+        stacks.append(list(reversed(new_col)))
+
+    move_re = r"move (\d+) from (\d+) to (\d+)"
+    instrs = [
+        [int(x) for x in re.findall(move_re, i)[0]] for i in instructions.splitlines()
+    ]
+    print("Part 1", part1(copy.deepcopy(stacks), instrs))
+    print("Part 2", part2(stacks, instrs))
+
+
+if __name__ == "__main__":
+    main()
