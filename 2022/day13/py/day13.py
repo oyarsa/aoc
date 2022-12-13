a6aea6106f62d5737658ea9b079b3ea3985dac5a
Italo Silva <italo@maleldil.com>
2022-12-13 08:37:18 +0000

    2022: Day 13, refactor to use match statement

--- 2022/day13/py/day13.py
+++ 2022/day13/py/day13.py
@@ -4,47 +4,37 @@ from functools import cmp_to_key
 
 
 def compare(left, right):
-    if isinstance(left, int) and isinstance(right, int):
-        if left < right:
+    match left, right:
+        case int(l), int(r):
+            return l - r
+        case [], r:
             return -1
-        if left > right:
+        case l, []:
             return 1
-        return 0
+        case [], []:
+            return 0
+        case int(l), _:
+            left = [l]
+        case _, int(r):
+            right = [r]
 
-    if isinstance(left, int):
-        left = [left]
-    if isinstance(right, int):
-        right = [right]
-
-    if not left and right:
-        return -1
-    if left and not right:
-        return 1
-    if not left and not right:
-        return 0
-
-    r = compare(left[0], right[0])
-    if r == 0:
-        return compare(left[1:], right[1:])
-    return r
-
-
-def is_correct(left, right):
-    return compare(left, right) == -1
+    if (r := compare(left[0], right[0])) != 0:
+        return r
+    return compare(left[1:], right[1:])
 
 
 def main():
     input = sys.stdin.read()
     pairs = [[literal_eval(y) for y in x.splitlines()] for x in input.split("\n\n")]
 
-    correct = [i + 1 for i, (l, r) in enumerate(pairs) if compare(l, r) == -1]
-    print("Part 1:", sum(correct))
+    correct = sum(i + 1 for i, (l, r) in enumerate(pairs) if compare(l, r) < 0)
+    print("Part 1:", correct)
 
-    all_pairs = [lst for pair in pairs for lst in pair] + [[[2]], [[6]]]
+    dividers = [[[2]], [[6]]]
+    all_pairs = [lst for pair in pairs for lst in pair] + dividers
     all_pairs.sort(key=cmp_to_key(compare))
-    div1 = all_pairs.index([[2]]) + 1
-    div2 = all_pairs.index([[6]]) + 1
-    print("Part 2:", div1 * div2)
+    div1, div2 = (all_pairs.index(div) for div in dividers)
+    print("Part 2:", (div1 + 1) * (div2 + 1))
 
 
 if __name__ == "__main__":
